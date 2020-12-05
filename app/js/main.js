cdebc6273257bb6177ee0a3fc4b823ca2adf9c79
Yurii Popsui <yp.onward@gmail.com>
2020-12-05 22:59:17 +0200

    Added Video-fashion and Product

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,13 @@ $(function() {
     arrows:false,
     fade: true 
   });
+
+  $('.star').rateYo({
+    rating: 3.6,
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+
+  });
 });
